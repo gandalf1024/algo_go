f05e4627946c2acdfd93a6053fa34193a2797746
v_vjunzhang <v_vjunzhang@tencent.com>
2021-01-18 15:34:12 +0800

    插入排序优化

--- two_sort/two_insertion_sort/insertion_sort.go
+++ two_sort/two_insertion_sort/insertion_sort.go
@@ -2,17 +2,9 @@ package two_insertion_sort
 
 func InsertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
-		for j := i; j > 0 && less(arr[j], arr[j-1]); j-- {
-			exch(arr, j, j-1)
+		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
 		}
 	}
 	return arr
-}
-
-func less(v, w int) bool {
-	return v-w < -1
-}
-
-func exch(arr []int, i, j int) {
-	arr[i], arr[j] = arr[j], arr[i]
 }
