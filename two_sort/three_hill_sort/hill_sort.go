22fd716ef4418c9956c94b575f6fbce7cfb11bc4
v_vjunzhang <v_vjunzhang@tencent.com>
2021-01-19 19:38:26 +0800

    希尔排序

---
+++ two_sort/three_hill_sort/hill_sort.go
@@ -0,0 +1,20 @@
+package three_hill_sort
+
+func HillSort(arr []int) []int {
+	N := len(arr)
+	h := 1
+	for h < N/3 {
+		h = 3*h + 1
+	}
+
+	for h >= 1 {
+		for i := h; i < N; i++ {
+			for j := i; j >= h && arr[j] < arr[j-h]; j -= h {
+				arr[j], arr[j-h] = arr[j-h], arr[j]
+			}
+		}
+		h = h / 3
+	}
+
+	return arr
+}
